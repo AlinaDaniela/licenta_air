703d7308c3fb2ae8a98d0b441b6c72eeb7224521
Alina Daniela <alinadanielagheorghe@gmail.com>
2013-06-19 02:20:25 +0300

    sync

--- config.php
+++ config.php
@@ -3,6 +3,7 @@ session_start();
 set_time_limit(0); 
 error_reporting(E_ALL); 
 header('Cache-control: private'); // IE 6 FIX
+date_default_timezone_set("Europe/Bucharest");
 
 $err = array(); //initializez vectorul err pe care o sa-l utilizez la fiecare formular. Daca e gol, se poate merge mai departe :)
 $salt = "f#@V)Hu^%Hgfds";
@@ -26,25 +27,16 @@ define("email_no_reply","alinadanielagheorghe@gmail.com");
 
 if(isSet($_GET['lang']))
 {
-$lang = $_GET['lang'];
-
-// register the session and set the cookie
-$_SESSION['lang'] = $lang;
-
-setcookie("lang", $lang, time() + (3600 * 24 * 30));
+    $lang = $_GET['lang'];
+    $_SESSION['lang'] = $lang;
+    setcookie("lang", $lang, time() + (3600 * 24 * 30));
 }
 else if(isSet($_SESSION['lang']))
-{
 	$lang = $_SESSION['lang'];
-}
 else if(isSet($_COOKIE['lang']))
-{
 	$lang = $_COOKIE['lang'];
-}
 else
-{
 	$lang = 'ro';
-}
 
 switch ($lang) {
   case 'en':
